7bf0d1cec7ef6998a450e8fea2e3ea078bbe39c2
Devi Pravallika <pravallika@gmail.com>
2026-03-12 19:00:42 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,22 @@
+// replace below firebase config with your own
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3yl0tdzaUysPSa0resHU6n3biCgqUw48",
+  authDomain: "netflix-clone-a2ed3.firebaseapp.com",
+  projectId: "netflix-clone-a2ed3",
+  storageBucket: "netflix-clone-a2ed3.firebasestorage.app",
+  messagingSenderId: "766339130434",
+  appId: "1:766339130434:web:9062293e3aa850820b39db",
+  measurementId: "G-902BJ7GKLV"
+};
+
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjk2NTYwMmRmNzdjMzA2MzE5YTViNDAyNzU0ZWEzZiIsIm5iZiI6MTc3MzMxMDIwNS4zNDcwMDAxLCJzdWIiOiI2OWIyOTBmZGEyNjBmMTU4OTg4MTc2YmUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.A7ZRafxzoeQwqSbVJzSLwFeKp2f5PbnqesS1ahMr5zo";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
